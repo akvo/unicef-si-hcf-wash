2b813d5cdbebc3ea3b3579dab31cc2abe208542e
wayangalihpratama <wgprtm@gmail.com>
2021-07-22 17:03:30 +0800

    [#1] Change composer version

--- ci/build-semaphore.sh
+++ ci/build-semaphore.sh
@@ -9,7 +9,7 @@ docker run \
        --volume "$(pwd):/home/tcakvo/public_html/si-hcf-wash" \
        --workdir /home/tcakvo/public_html/si-hcf-wash \
        --entrypoint /bin/sh \
-       composer -c 'composer install'
+       composer:1.10.17 -c 'composer install'
 
 echo 'MIX_PUBLIC_URL="https://si-hcf-wash.tc.akvo.org"' > .env
 
