dc563b9b1cc2f287838979bd48dc18710e703b76
wayangalihpratama <wgprtm@gmail.com>
2021-07-22 16:41:59 +0800

    [#1] Setup build & deploy, add build status

---
+++ README.md
@@ -0,0 +1,3 @@
+[![Build Status](https://akvo.semaphoreci.com/badges/unicef-si-hcf-wash/branches/master.svg?style=shields)](https://akvo.semaphoreci.com/projects/unicef-si-hcf-wash)
+
+# UNICEF - SI HCF WASH Project
---
+++ ci/build-semaphore.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -eu
+
+#cp .env.prod .env
+
+docker run \
+       --rm \
+       --volume "$(pwd):/home/tcakvo/public_html/si-hcf-wash" \
+       --workdir /home/tcakvo/public_html/si-hcf-wash \
+       --entrypoint /bin/sh \
+       composer -c 'composer install'
+
+echo 'MIX_PUBLIC_URL="https://si-hcf-wash.tc.akvo.org"' > .env
+
+docker run \
+       --rm \
+       --volume "$(pwd):/home/tcakvo/public_html/si-hcf-wash" \
+       --workdir "/home/tcakvo/public_html/si-hcf-wash" \
+       --entrypoint /bin/sh \
+       node:14-alpine -c 'npm i && npm run prod'
+
